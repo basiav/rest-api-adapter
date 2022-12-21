a233bd45f292e18ec40b5f1e8fb4d14a179e2dcd
basiav <82549088+basiav@users.noreply.github.com>
2022-12-21 23:27:44 +0100

    Some code clean-up

--- routes/index.js
+++ routes/index.js
@@ -3,19 +3,17 @@ const router = express.Router();
 
 const axios = require("axios");
 
-// const readline = require('node:readline');
-// const { stdin: input, stdout: output } = require('node:process');
 const readline = require('readline');
+const rl = readline.createInterface(process.stdin, process.stdout);
+
+const URL_BASE = 'https://jsonplaceholder.typicode.com/';
+const COMMENTS_LIMIT = 5;
+let userId;
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
     main(res, req);
-    // res.render('index', { title: 'Express' });
 });
-
-const urlBase = 'https://jsonplaceholder.typicode.com/';
-
-const rl = readline.createInterface(process.stdin, process.stdout);
 
 const getReadlinePromise = (query) => {
   return new Promise((resolve, reject) => {
@@ -25,8 +23,9 @@ const getReadlinePromise = (query) => {
   });
 }
 
-let userId;
-const commentsLimit = 5;
+const config = {
+    headers: {"Accept-Encoding": "gzip,deflate,compress"}
+};
 
 // const getUserId =  async (Type) => {
 //   const query = "Enter userId";
@@ -37,6 +36,32 @@ const commentsLimit = 5;
 //
 // getUserId(Number)
 //     .then((r) => console.log("R ", r));
+
+const applyCommentsLimit = (commentsByPostIdUrl) => {
+    return `${commentsByPostIdUrl}&_limit=${COMMENTS_LIMIT}`;
+};
+
+const getLimitedCommentsUrl = (commentsByPostIdUrl) => {
+    return applyCommentsLimit(commentsByPostIdUrl);
+};
+
+const getCommentsByPostIdUrl = (postId) => {
+    return `${URL_BASE}comments?postId=${postId}`;
+};
+
+const getCommentsEndpoint = (postId) => {
+    return getLimitedCommentsUrl(getCommentsByPostIdUrl(postId))
+}
+
+const getAxiosRequest = (endpoint) => {
+    return axios.get(endpoint, config);
+};
+
+const populatePostMap = (posts, postMap) => {
+    posts.forEach(post => {
+        postMap.set(post.id, post);
+    });
+}
 
 const main =  async (res, req) => {
   const type = Number;
@@ -50,53 +75,23 @@ const main =  async (res, req) => {
   userId = result;
 
   const userIdSuffix = `users/${userId}`;
-  const postsByUserIdUrl = `${urlBase}${userIdSuffix}/posts`;
-  console.log(postsByUserIdUrl);
-
-  const config = {
-    headers: {"Accept-Encoding": "gzip,deflate,compress"}
-  };
-
-  const applyCommentsLimit = (commentsByPostIdUrl) => {
-      return `${commentsByPostIdUrl}&_limit=${commentsLimit}`;
-  };
-
-  const getLimitedCommentsUrl = (commentsByPostIdUrl) => {
-      return applyCommentsLimit(commentsByPostIdUrl);
-  };
-
-  const getCommentsByPostIdUrl = (postId) => {
-      return `${urlBase}comments?postId=${postId}`;
-  };
-
-  const getCommentsEndpoint = (postId) => {
-      return getLimitedCommentsUrl(getCommentsByPostIdUrl(postId))
-  }
-
-  const getAxiosRequest = (endpoint) => {
-        return axios.get(endpoint, config);
-  };
-
+  const postsByUserIdUrl = `${URL_BASE}${userIdSuffix}/posts`;
+  
   const posts = axios.get(postsByUserIdUrl, config)
       .then(posts => {
         posts = posts.data;
 
-        // let commentsByPostId = [];
-        //   let commentsByPostId = new Map();
-          // let comments
-          let postMap = new Map();
-          let resJson = [];
+        let postMap = new Map();
+        let resJson = [];
 
-          posts.forEach(post => {
-              postMap.set(post.id, post);
-          });
+        populatePostMap(posts, postMap);
 
-          const parallelAxiosCommentsRequests = posts.map((post) => getAxiosRequest(getCommentsEndpoint(post.id)));
+        const parallelAxiosCommentsRequests = posts.map((post) => getAxiosRequest(getCommentsEndpoint(post.id)));
 
         axios.all(parallelAxiosCommentsRequests)
             .then(axios.spread((...commentsResponses) => {
                 if (commentsResponses.length !== postMap.size) {
-                    console.log("ERROR WITH CAPACITIES");
+                    console.log(`Not every post of user ${userId} has been commented`);
                 }
 
                 commentsResponses = commentsResponses.map((response) => response.data);
@@ -104,51 +99,25 @@ const main =  async (res, req) => {
                 commentsResponses.forEach((response, index) => {
                     console.log(index);
                     let postId = index + 1;
-                  if (response) {
-                      let post = postMap.get(postId);
-                      if (post) {
+                    if (response) {
+                        let post = postMap.get(postId);
+                        if (post) {
                           post = {...post, "comments": response};
                           resJson.push(post);
-                      }
-                  }
-
-              });
+                        }
+                    }
+                });
               res.json(resJson);
             }))
             .catch(errors => {
-              console.log("ERROR ", errors);
+              console.log("Error while fetching comments... ", errors);
             });
 
-
-
       })
-      .catch((err) => console.log("ERROR ", err));
+      .catch((error) => {
+          console.log(`Error while fetching user's ${userId} posts`, error);
+      });
 
 }
-
-// main();
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 module.exports = router;
