0293986036f69fbf64623ee17b0d649b6385db14
basiav <82549088+basiav@users.noreply.github.com>
2022-12-21 16:21:24 +0100

    Fetching posts with given userId

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
+const axios = require("axios");
+
 // const readline = require('node:readline');
 // const { stdin: input, stdout: output } = require('node:process');
 const readline = require('readline');
@@ -24,17 +26,42 @@ const getReadlinePromise = (query) => {
 
 let userId;
 
-const getUserId =  async (Type) => {
+// const getUserId =  async (Type) => {
+//   const query = "Enter userId";
+//   const result = await getReadlinePromise(query).then((id) => Type(id));
+//   rl.close();
+//   return result;
+// }
+//
+// getUserId(Number)
+//     .then((r) => console.log("R ", r));
+
+const main =  async () => {
+  const type = Number;
   const query = "Enter userId";
-  const result = await getReadlinePromise(query).then((id) => Type(id));
+
+  const result = await getReadlinePromise(query).then((id) => type(id));
+
+
   rl.close();
-  return result;
+
+  userId = result;
+
+  const userIdSuffix = `users/${userId}`;
+  const postsByUserIdUrl = `${urlBase}${userIdSuffix}/posts`;
+  console.log(postsByUserIdUrl);
+
+  const config = {
+    headers: {"Accept-Encoding": "gzip,deflate,compress"}
+  };
+
+  const posts = axios.get(postsByUserIdUrl, config)
+      .then(r => console.log(r.data))
+      .catch((err) => console.log("ERROR ", err));
+
 }
 
-getUserId(Number)
-    .then((r) => console.log("R ", r));
-
-
+main();
 
 
 
